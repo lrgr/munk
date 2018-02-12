4fa7b82561594ccc709f44259cc38a54ffe2dc07
Jason Fan <jason.fan.74@gmail.com>
2018-02-12 14:27:09 -0500

    removing data building code, replaced with scripts to download from umd instead

--- data/homologs/swap_cols.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sys
-
-f = sys.argv[1]
-out = sys.argv[2]
-df = pd.read_csv(f, delimiter='\t', index_col=False, header=None)
-df=df.reindex(columns=[1,0])
-df.to_csv(out, sep='\t', index=False, header=False)
-
