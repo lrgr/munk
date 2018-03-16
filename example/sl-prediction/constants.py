068771fc2e800102701d114379675b679de887a7
Max Leiserson <mdml@cs.umd.edu>
2018-03-16 09:21:04 -0400

    clean up CORAL alignment/prediction code, and add normalization for CORAL-ing within target

--- example/sl-prediction/constants.py
+++ example/sl-prediction/constants.py
@@ -15,5 +15,7 @@ GENES = 'genes'
 GENE_PAIRS = 'pairs'
 
 # Normalization choices
-CORAL_NORM = 'coral'
+CORAL_SRC_NORM = 'coral-src' # align source "into" target
+CORAL_TGT_NORM = 'coral-tgt' # align target non-landmarks into target landmarks
 NONE_NORM  = 'none'
+NORM_MODES = [ CORAL_SRC_NORM, CORAL_TGT_NORM, NONE_NORM ]
